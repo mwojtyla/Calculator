c728e00a7cb8e42fea540c525c2eedb59c02cf86
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2022-10-29 00:41:45 +0200

    Calculator commit

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -4,7 +4,15 @@ public class Calculator {
     public int addAToB (int a, int b){
         return a+b;
     }
-    public void subAFromB (int a, int b){
+    public int subAFromB (int a, int b){
         return b-a;
+    }
+
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        int result1 = calculator.addAToB(8,9);
+        int result2 = calculator.subAFromB(2,15);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 }
