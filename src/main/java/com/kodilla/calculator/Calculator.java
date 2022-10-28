f638a7583b40b24755e9ee4ca8ae93877c7a8d8f
Martyna Wojtyla <martyna.wojtyla@gmail.com>
2022-10-28 23:52:03 +0200

    Calculator commit

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.calculator;
+
+public class Calculator {
+    public int addAToB (int a, int b){
+        return a+b;
+    }
+    public void subAFromB (int a, int b){
+        return b-a;
+    }
+}
